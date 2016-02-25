41f13a665c7ab5897a376f1097e896b1353e3c0f
Vivek Ramamoorthy <vivek.ramamoorthy@hpe.com>
2016-02-25 11:03:05 -0800

    Adding start/stop and rate library funcion

--- lib/topology_lib_tcpdump/library.py
+++ lib/topology_lib_tcpdump/library.py
@@ -22,15 +22,48 @@ topology_lib_tcpdump communication library implementation.
 from __future__ import unicode_literals, absolute_import
 from __future__ import print_function, division
 
+from re import match
+
 # Add your library functions here.
 
+def tcpdump_rate(sw)
 
-def your_function_here():
-    """
-    Document your function here.
-    """
-    pass
+    capture = sw('cat /tmp/interface.cap'.format(**locals()),
+                 shell='bash_swns')
+    with open ("/tmp/interface.cap", "r") as pcap:
+    capture =  pcap.read()
+
+    packets = data.splitlines()
+    total_packets = len(packets)
+    last_packet = packets[len(packets)-1]
+    fields = last_packet.split()
+    timestamp = datetime.datetime.strptime(fields[0], '%H:%M:%S.%f').time()
+    msec = (timestamp.hour *60*60 + timestamp.minute * 60 + timestamp.second) * 1000 + (timestamp.microsecond/1000)
+    rate = total_packets * 1000 / msec
+    return rate
+ 
+def tcpdump_capture_interface(sw, options, interface_id, wait_time):
+    cmd_output = sw('tcpdump -D'.format(**locals()),
+                    shell='bash_swns')
+    interface_re = (r'(?P<linux_interface>\d)\.' + interface_id +
+                    r'\s[\[Up, Running\]]')
+    re_result = re.search(interface_re, cmd_output)
+    assert re_result
+    result = re_result.groupdict()
+
+    sw('tcpdump -ni ' + result['linux_interface'] +
+        options + '-ttttt'
+        '> /tmp/interface.cap 2>&1 &'.format(**locals()),
+        shell='bash_swns')
+    time.sleep(wait_time)
+    sw('killall tcpdump'.format(**locals()),
+        shell='bash_swns')
+    capture = sw('cat /tmp/interface.cap'.format(**locals()),
+                 shell='bash_swns')
+    sw('rm /tmp/interface.cap'.format(**locals()),
+       shell='bash_swns')
+    return capture
 
 __all__ = [
-    'your_function_here'
+    'tcpdump_capture_interface'
 ]
